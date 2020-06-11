2d3595a1a48b4c7186c5e18b932c3fa6f0e92704
Ricardo Pineda Lazcano <urielink81@gmail.com>
2020-06-11 14:43:04 -0500

    refactorizar metodo de llamado de scripts.

--- inc/themeOptimize.php
+++ inc/themeOptimize.php
@@ -10,6 +10,166 @@
  * @package ekiline
  */
 
+$optimizeCSS = true;
+if( $optimizeCSS === true ){
+    add_filter( 'style_loader_tag',  'ekiline_change_tag', 10, 4 );
+    add_action( 'wp_enqueue_scripts', 'ekiline_print_localize' );
+    add_action( 'wp_footer', 'ekiline_load_allCss_js', 100);
+}
+
+/**
+ * Optimizar el llamdo de los estilos.
+ * Decidir que estilos pueden permanecer y que otros no.
+ */
+
+    function ekiline_choose_styles(){
+        // $discardCss = array('bootstrap-4');
+        $discard_styles = array(); 
+        return $discard_styles;
+    }
+
+    function ekiline_wpqueued_styles(){
+        // Estilos en el sistema
+        global $wp_styles; 
+        $all_styles = array();
+        foreach( $wp_styles->queue as $csshandle ) {    	
+            $all_styles[] = $csshandle;   
+        } 
+        return $all_styles;
+    }
+
+    function ekiline_filter_styles(){
+        // Filtrar los estilos
+        $load_css = array_diff( ekiline_wpqueued_styles(), ekiline_choose_styles() );
+        $final_styles = $load_css;
+        return $final_styles;
+    }
+
+/**
+ * Transformar etiquetas de estilo en preloads
+ */
+
+function ekiline_change_tag( $tag, $handle, $src  ) {
+    foreach( ekiline_filter_styles() as $pre_style ) {
+        if ( $pre_style === $handle ) {
+            $tag = '<link rel="preload" as="style" href="' . esc_url( $src ) . '">'."\n";            
+        }
+    }
+    return $tag;
+}
+// add_filter( 'style_loader_tag',  'ekiline_change_tag', 10, 4 );
+
+
+/**
+ * Crear variables de cada estilo filtrado en js.
+ */
+function ekiline_styles_localize(){
+
+        global $wp_styles; 
+        $load_css_from = ekiline_filter_styles();        
+        $the_styles = array();
+
+        foreach( $load_css_from as $handler) {
+
+            /* Crear diccionario: 
+            * sobrescribir url de cada CSS en caso de ser relativa al sistema.
+            */
+
+            $srcUrl = $wp_styles->registered[$handler]->src;
+            $siteurl   = get_site_url();
+                $hasSiteurl = strpos($srcUrl, $siteurl);
+
+            if ($hasSiteurl === false){
+                $srcUrl = get_site_url() . $wp_styles->registered[$handler]->src;
+            } 
+
+            $the_styles[] = array( 'id' => $handler, 'src' => $srcUrl, 'media' => $wp_styles->registered[$handler]->args );   
+
+            // Para deshabilitar estilos: wp_dequeue_style($handler);
+
+        }   
+                
+        return $the_styles;
+        
+}  
+function ekiline_print_localize(){
+    wp_localize_script( 'ekiline-layout', 'allCss', ekiline_styles_localize() );     
+}
+// add_action( 'wp_enqueue_scripts', 'ekiline_print_localize' );
+
+
+function ekiline_load_allCss_js(){ ?>
+    <script>
+    jQuery(document).ready( function($){
+        // variable php
+        if ( allCss != null ){
+            var obj = allCss;	
+            // crear un estilo por cada ruta extríada.
+            
+            $.each( obj, function( key, value ) {
+                        
+                var head = $('head');
+                var wpcss = head.find('style[id="ekiline-inline-style"]'); 
+                var cssinline = head.find('style:last');
+                var ultimocss = head.find('link[rel="stylesheet"]:last');
+                var linkCss = $('<link/>',{ 'rel':'stylesheet', 'id':value.id, 'href':value.src, 'media':value.media });
+            
+                // En caso de de encontrar una etiqueta de estilo ó link ó nada inserta el otro estilo css, 
+            
+                    if (wpcss.length){ 
+                        wpcss.before(linkCss); 
+                    } else if (cssinline.length){ 
+                        cssinline.before(linkCss); 
+                    } else if (ultimocss.length){ 
+                        ultimocss.before(linkCss); 
+                    } else { 
+                        head.append(linkCss); 
+                    }		
+                                
+            });				                
+        }	            
+    });     
+    </script>
+<?php }
+// add_action( 'wp_footer', 'ekiline_load_allCss_js', 100);
+
+
+
+/*** otros métodos que ayudan ***/
+
+/* 
+ * Defer / Async scripts.
+ * https://developer.wordpress.org/reference/hooks/script_loader_src/
+ * http://hookr.io/filters/script_loader_src/
+ */
+
+function add_defer_attribute($tag, $handle) {
+    // add script handles to the array below
+    $scripts_to_defer = array( 'popper-script', 'bootstrap-script', 'ekiline-swipe', 'ekiline-layout' );
+    
+    foreach($scripts_to_defer as $defer_script) {
+       if ($defer_script === $handle) {
+          return str_replace(' src', ' defer="defer" src', $tag);
+       }
+    }
+    return $tag;
+ }
+ add_filter('script_loader_tag', 'add_defer_attribute', 10, 2);
+
+
+ function add_async_attribute($tag, $handle) {
+    // add script handles to the array below
+    $scripts_to_async = array();
+    
+    foreach($scripts_to_async as $async_script) {
+       if ($async_script === $handle) {
+          return str_replace(' src', ' async="async" src', $tag);
+       }
+    }
+    return $tag;
+ }
+ add_filter('script_loader_tag', 'add_async_attribute', 10, 2);
+
 
 
 
@@ -41,101 +201,3 @@ if ( $optimizeCSS == true && ! is_customize_preview() ){
 
 }
 */
-
-function ekiline_loadcss(){
-    $optimizeCSS = true;
-    if( $optimizeCSS != true ) return;
-    wp_localize_script( 'ekiline-layout', 'allCss', ekiline_reloadCSS() ); 
-    add_action( 'wp_footer', 'ekiline_loadAllCss', 100);
-}
-add_action( 'wp_enqueue_scripts', 'ekiline_loadcss' );
-
-
-function ekiline_reloadCSS(){
-
-    $params = null;
-    
-    global $wp_styles; 
-
-        $allcss = array();
-        foreach( $wp_styles->queue as $csshandle ) {    	
-            $allcss[] = $csshandle;   
-        } 
-        /* Permitir la carga de estilos que tienen dependencia o prioridad, 
-        * feature, crear una lista para seleccionar cada objeto.
-        * $allowcss = array('photoswipe-default-skin','woocommerce-inline');
-        */
-            $allowcss = array();	
-            $load_css = array_diff( $allcss, $allowcss );        
-        
-        $cssDic = array();
-        foreach( $load_css as $handle) {
-
-            /* Crear diccionario: 
-            * sobrescribir url de cada CSS en caso de ser relativa al sistema.
-            */
-
-            $srcUrl = $wp_styles->registered[$handle]->src;
-            $siteurl   = get_site_url();
-                $hasSiteurl = strpos($srcUrl, $siteurl);
-
-            if ($hasSiteurl === false){
-                $srcUrl = get_site_url() . $wp_styles->registered[$handle]->src;
-            } 
-
-            $cssDic[] = array( 'id' => $handle, 'src' => $srcUrl, 'media' => $wp_styles->registered[$handle]->args );   
-
-            //deshabilitar cada estilo
-            wp_dequeue_style($handle);
-
-        }   
-
-        $params = $cssDic; 
-                
-        return $params;
-        
-}  
-
-
-
-
-
-
-function ekiline_loadAllCss(){ ?>
-
-<script>
-jQuery(document).ready( function($){
-    // variable php
-    if ( allCss != null ){
-        var obj = allCss;	
-        // crear un estilo por cada ruta extríada.
-        
-        $.each( obj, function( key, value ) {
-                    
-            var head = $('head');
-            var wpcss = head.find('style[id="ekiline-inline-style"]'); 
-            var cssinline = head.find('style:last');
-            var ultimocss = head.find('link[rel="stylesheet"]:last');
-            var linkCss = $('<link/>',{ 'rel':'stylesheet', 'id':value.id, 'href':value.src, 'media':value.media });
-        
-            // En caso de de encontrar una etiqueta de estilo ó link ó nada inserta el otro estilo css, 
-        
-                if (wpcss.length){ 
-                    wpcss.before(linkCss); 
-                } else if (cssinline.length){ 
-                    cssinline.before(linkCss); 
-                } else if (ultimocss.length){ 
-                    ultimocss.before(linkCss); 
-                } else { 
-                    head.append(linkCss); 
-                }		
-                            
-        });				                
-    }	            
-});     
-</script>
-
-<?php 
-}
-
-
