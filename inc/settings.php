874f4c8934c61c90145f3dd080fce0a87ef7f8ee
Ricardo Pineda Lazcano <urielink81@gmail.com>
2020-07-08 18:10:26 -0500

    ajustes de escapado de caracteres

--- inc/settings.php
+++ inc/settings.php
@@ -27,7 +27,8 @@ function theme_settings_page()
 
 function add_theme_menu_item()
 {
-	add_menu_page("Theme Panel", "Theme Panel", "manage_options", "theme-panel", "theme_settings_page", null, 99);
+	// add_menu_page("Theme Panel", "Theme Panel", "manage_options", "theme-panel", "theme_settings_page", null, 99);
+	add_theme_page("Theme Panel", "Theme Panel", "manage_options", "theme-panel", "theme_settings_page", null, 99);
 }
 
 add_action("admin_menu", "add_theme_menu_item");
--- template-parts/custom-header.php
+++ template-parts/custom-header.php
@@ -14,7 +14,7 @@ if ( !get_header_image() ) return;
 	<div class="wp-block-cover has-background-dim-20 has-background-dim has-parallax rounded bg-deep <?php echo ( get_theme_mod('ekiline_headerCustomWidth') != '' ) ? '' : 'alignfull'; ?>" style="background-image:url('<?php echo ekiline_header_image(); ?>');">
 
 	<?php if ( get_theme_mod('ekiline_video') && is_front_page() ){?>
-		<video class="wp-block-cover__video-background" autoplay="" muted="" loop="" src="<?php echo get_theme_mod('ekiline_video');?>" poster="<?php echo ekiline_header_image(); ?>"></video>
+		<video class="wp-block-cover__video-background" autoplay="" muted="" loop="" src="<?php echo esc_url(get_theme_mod('ekiline_video'));?>" poster="<?php echo ekiline_header_image(); ?>"></video>
 	<?php } ?>
 
 		<div class="col-md-6 px-0 mx-auto text-center">
