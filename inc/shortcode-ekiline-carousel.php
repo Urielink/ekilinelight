bc215f4648d7bbc91d2411c50781080e2a10a781
Ricardo Pineda Lazcano <urielink81@gmail.com>
2020-11-27 00:27:17 -0600

    ajustes css para el carrusel. nuevos parametros para personalizar el carrusel.

--- inc/shortcode-ekiline-carousel.php
+++ inc/shortcode-ekiline-carousel.php
@@ -12,7 +12,7 @@
  *
  * Default posts, solo agregar ids, limites y columnas opcionales.
  * Extraer bloque con ubicacion y nombre.
- * [ekiline-carousel id=category amount=n columns=1,2,3,4,o 6 block=location/name sort=@REF]
+ * [ekiline-carousel id=category amount=n columns=1,2,3,4,o 6 block=location/name sort=@REF mixed=true]
  *
  * Opcional solo imagenes, agregar id, limites y columnas opcionales.
  * [ekiline-carousel type="images" id=image amount=n columns=1,2,3,4,o 6]
@@ -25,12 +25,18 @@ function ekiline_shortcode_carousel( $atts = [] ) {
 
 	$atts = shortcode_atts(
 		array(
-			'type'    => 'posts', // Default posts, or images.
-			'id'      => null, // Default none, image ids, category ids.
-			'amount'  => '3', // Default 3.
-			'orderby' => null, // Default 3.
-			'columns' => null, // Default single view.
-			'block'   => null, // Find a block.
+			'type'       => 'posts', // Default posts, or images.
+			'id'         => null, // Default none, image ids, category ids.
+			'amount'     => '3', // Default 3.
+			'orderby'    => null, // Default 3.
+			'columns'    => null, // Default single view.
+			'block'      => null, // Find a block.
+			'mixed'      => null, // Dont load post if image or block not exist.
+			'control'    => null, // Show carousel controls.
+			'indicators' => null, // Show carousel indicators.
+			'auto'       => null, // Show carousel indicators.
+			'time'       => null, // Set time interval between slides.
+			'animation'  => null, // Set time interval between slides.
 		),
 		$atts,
 		'ekiline-carousel'
@@ -39,7 +45,7 @@ function ekiline_shortcode_carousel( $atts = [] ) {
 	// Obtener ids.
 	$id_arr = explode( ',', $atts['id'] );
 	// Default posts.
-	$carousel = ekiline_carousel_posts( $atts['amount'], $id_arr, $atts['block'], $atts['orderby'] );
+	$carousel = ekiline_carousel_posts( $atts['amount'], $id_arr, $atts['block'], $atts['orderby'], $atts['mixed'] );
 	// Condicion para images.
 	if ( 'images' === $atts['type'] ) {
 		$carousel = ekiline_carousel_images( $id_arr );
@@ -48,7 +54,7 @@ function ekiline_shortcode_carousel( $atts = [] ) {
 	$columns = ( in_array( $atts['columns'], [ '2', '3', '4', '6' ], true ) ) ? ' carousel-multiple x' . $atts['columns'] : '';
 	// Obtener HTML y combinar con funciones previas.
 	ob_start();
-	ekiline_carousel_html( $carousel, $columns );
+	ekiline_carousel_html( $carousel, $columns, $atts['control'], $atts['indicators'], $atts['auto'], $atts['time'], $atts['animation'] );
 	return ob_get_clean();
 }
 // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.plugin_territory_add_shortcode
@@ -65,9 +71,10 @@ add_shortcode( 'ekiline-carousel', 'ekiline_shortcode_carousel' );
  * @param array  $cat category ids or slug.
  * @param string $findblock block/name, to find and parse in slide.
  * @param string $orderby date/rand/etc, sort slides.
+ * @param string $mixed allow to show thumbnails and blocks.
  * @return array query data.
  */
-function ekiline_carousel_posts( $ppp = 3, $cat = array(), $findblock = null, $orderby = 'date' ) {
+function ekiline_carousel_posts( $ppp = 3, $cat = array(), $findblock = null, $orderby = 'date', $mixed = null ) {
 
 	$carousel = array();
 
@@ -100,15 +107,16 @@ function ekiline_carousel_posts( $ppp = 3, $cat = array(), $findblock = null, $o
 			}
 
 			if ( $findblock ) {
+
+				if ( 'true' !== $mixed ) {
+					// Reset array, ignorar la informacion acumulada, solo mantener la nueva.
+					$info = array();
+				}
+
 				$blocks = parse_blocks( get_the_content() );
 				foreach ( $blocks as $block ) {
 					if ( $block['blockName'] === $findblock ) {
 						$info['block'] = render_block( $block );
-					} else {
-						// Si no tiene ninguna imagen blanquear el array, definir mejor el caso de uso.
-						if ( ! has_post_thumbnail() ) {
-							$info = array();
-						}
 					}
 				}
 			}
@@ -155,23 +163,35 @@ function ekiline_carousel_images( $ids = array() ) {
  *
  * @param array  $carousel recibe los datos de loop previos.
  * @param string $columns obtiene la clase css que extiende la vista del carrusel.
+ * @param string $control opcion, ocultar controles = false.
+ * @param string $indicators opcion, ocultar indicadores = false.
+ * @param string $auto opcion, no iniciar carrusel = false.
+ * @param string $time opcion, milisegundos para las transiciones del carrusel = 5000.
+ * @param string $animation opcion, = fade, vertical.
  */
-function ekiline_carousel_html( $carousel, $columns ) {
+function ekiline_carousel_html( $carousel, $columns, $control, $indicators, $auto, $time, $animation ) {
 
 	if ( $carousel ) {
-		$uniq_id = 'carousel_module_' . wp_rand( 1, 99 );
+		$uniq_id   = 'carousel_module_' . wp_rand( 1, 99 );
+		$auto      = ( 'false' !== $auto ) ? ' data-ride="carousel"' : '';
+		$time      = ( $time ) ? ' data-interval="' . $time . '"' : '';
+		$animation = ( $animation ) ? ' carousel-' . $animation : '';
 		?>
 
-		<div id="<?php echo esc_attr( $uniq_id ); ?>" class="carousel slide<?php echo esc_attr( $columns ); ?>" data-ride="false">
+		<div id="<?php echo esc_attr( $uniq_id ); ?>" class="carousel slide<?php echo esc_attr( $columns . $animation ); ?>"<?php echo wp_kses_post( $auto . $time ); ?>>
 
-			<ol class="carousel-indicators">
-				<?php
-				foreach ( $carousel as $index => $indicator ) {
-					$active = ( 0 === $index ) ? 'active' : '';
-					?>
-					<li data-target="#<?php echo esc_html( $uniq_id ); ?>" data-slide-to="<?php echo esc_attr( $index ); ?>" class="<?php echo esc_attr( $active ); ?>"></li>
-				<?php } ?>
-			</ol>
+			<?php if ( 'false' !== $indicators ) { ?>
+
+				<ol class="carousel-indicators">
+					<?php
+					foreach ( $carousel as $index => $indicator ) {
+						$active = ( 0 === $index ) ? 'active' : '';
+						?>
+						<li data-target="#<?php echo esc_html( $uniq_id ); ?>" data-slide-to="<?php echo esc_attr( $index ); ?>" class="<?php echo esc_attr( $active ); ?>"></li>
+					<?php } ?>
+				</ol>
+
+			<?php } ?>
 
 			<div class="carousel-inner">
 				<?php
@@ -221,14 +241,18 @@ function ekiline_carousel_html( $carousel, $columns ) {
 				<?php } ?>
 			</div>
 
-			<a class="carousel-control-prev" href="#<?php echo esc_html( $uniq_id ); ?>" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#<?php echo esc_html( $uniq_id ); ?>" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+			<?php if ( 'false' !== $control ) { ?>
+
+				<a class="carousel-control-prev" href="#<?php echo esc_html( $uniq_id ); ?>" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#<?php echo esc_html( $uniq_id ); ?>" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+
+			<?php } ?>
 
 		</div>
 		<?php
