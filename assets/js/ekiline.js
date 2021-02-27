c838ebce6668cd1c52a392707a60e304db0972e1
Ricardo Pineda Lazcano <urielink81@gmail.com>
2021-02-27 00:30:53 -0600

    Si pude!!! \o/ carrusel actualizado a JS

--- assets/js/ekiline.js
+++ assets/js/ekiline.js
@@ -78,6 +78,7 @@ jQuery(document).ready(function( $ ) {
 			}
 			// Items envoltorio.
 			$(this).find('.carousel-item').each(function(){
+				// console.log($(this).children());
 				$(this).children().wrapAll('<figure class="col-md-' + item + '">','</figure>');
 			});
 			// Loop grupos.
@@ -101,6 +102,114 @@ jQuery(document).ready(function( $ ) {
 		});
 
 	}
-	transformarCarrusel('.carousel-multiple');
+	// transformarCarrusel('.carousel-multiple');
 
 });
+
+
+window.transformarCarruselNativo = function(carrusel){
+
+	// Si no hay carrusel cancelar todo.
+	var loaditem = document.querySelector(carrusel);
+	if ( !loaditem || 0 < loaditem.getElementsByTagName('figure').length ) {
+		return;
+	}
+
+	// Funcion envoltorio (Wrapper).
+	function envolver(fuente,col){
+		// console.log(fuente)
+		var hijos = fuente.children;
+		// var padre = fuente.parentNode;//prueba
+		// crear envoltorio
+		var wrapper = document.createElement('figure');
+			wrapper.className = 'col-md-' + col;
+		// envolver los hijos.
+		for (var i = hijos.length - 1; i >= 0; i--) {
+			// padre.appendChild(hijos[i]);//prueba
+			wrapper.appendChild(hijos[i]);
+		};
+		fuente.appendChild(wrapper);
+	}
+
+	// Si hay carrusel,
+	var siCarruseles = document.querySelectorAll(carrusel);
+
+	// // Cuantos son.
+	// console.log('cuantosson');
+	// console.log(siCarruseles.length);
+
+	// Modificar cada uno
+	Array.prototype.forEach.call(siCarruseles, function(unCarrusel, i){
+
+		// // objeto e indice.
+		// console.log('quees');
+		// console.log(unCarrusel);
+		// console.log('contar');
+		// console.log(i);
+
+		// Vistas, columnas y grupo.
+		var params = [ ['x2','6','0'],['x3','4','1'],['x4','3','2'],['x6','2','4'] ];
+		var view, item;
+		// Envoltorio extra para agrupar.
+		for ( var i = 0; i < params.length; i++ ) {
+			// //atributos por clase.
+			// console.log(params[i][0]);
+			if ( unCarrusel.classList.contains(params[i][0]) ) {
+				item = params[i][1];
+				view = params[i][2];
+			}
+		}
+
+		// //resultado de seleccion por carrusel
+		// console.log(item);
+		// console.log(view);
+
+		// // Carrusel padre.
+		// console.log(unCarrusel);
+
+		// Items para envoltorio.
+		hijosCarrusel = unCarrusel.querySelectorAll('.carousel-item');
+
+		// Carrusel hijo.
+		// console.log(hijosCarrusel);
+
+		// Envoltorio por item.
+		Array.prototype.forEach.call(hijosCarrusel, function(el,i){
+			envolver(el,item);
+		});
+
+		// Loop grupos.
+		Array.prototype.forEach.call(hijosCarrusel, function(el, i){
+			// Copiar el primer slide y agregarlo.
+			var next = el.nextElementSibling;
+			if ( !next ) {
+				next = el.parentNode.children[0];
+			}
+
+			// Elemento siguiente.
+			// console.log(next);
+
+			// Elemento siguiente clonar.
+			// console.log(next.children[0]);
+			// console.log(el.parentNode.children[i]);
+
+			var firstChildClone = next.children[0].cloneNode(true);
+			var firstChildSet = el.parentNode.children[i];
+			firstChildSet.appendChild(firstChildClone);
+
+			// Agrupar slides (view).
+			// console.log(view)
+
+			for ( var i=0;i<view;i++ ) {
+				next = next.nextElementSibling;
+				if ( !next ) {
+					next = el.parentNode.children[0];
+				}
+				firstChildClone = next.children[0].cloneNode(true);
+				firstChildSet.appendChild(firstChildClone);
+			}
+
+		});
+	});
+};
+transformarCarruselNativo('.carousel-multiple');
