f60010342166d6aa6eb0a0cbce0bec76cdbaa02d
Ricardo Pineda Lazcano <urielink81@gmail.com>
2021-03-29 13:41:33 -0600

    refact html. ocupar clase smooth para scroll suave.

--- template-parts/custom-header.php
+++ template-parts/custom-header.php
@@ -22,7 +22,7 @@
 		<div class="headline position-relative <?php echo ( ! get_theme_mod( 'ekiline_headerCustomWidth' ) ) ? 'container' : 'w-100'; ?>">
 			<h1 class="title display-3">
 				<?php echo wp_kses_post( ekiline_custom_header_content( 'title' ) ); ?>
-				<a href="#primary" class="skip-link small">&#8595;</a>
+				<a href="#primary" class="skip-link smooth small">&#8595;</a>
 			</h1>
 			<p class="lead">
 				<?php echo wp_kses_post( ekiline_custom_header_content( 'text' ) ); ?>
