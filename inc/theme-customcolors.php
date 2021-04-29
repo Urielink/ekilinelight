9a4513489cabd79e0b68425ea131c4cf347514bc
Ricardo Pineda Lazcano <urielink81@gmail.com>
2021-04-28 23:17:18 -0500

    fix: mostrar colores a menos que existan

--- inc/theme-customcolors.php
+++ inc/theme-customcolors.php
@@ -290,24 +290,24 @@ add_action( 'customize_controls_enqueue_scripts', 'ekiline_themecustomizer_js' )
 function ekiline_page_elements() {
 
 	$ekiline_lmnt  = '';
-	$ekiline_lmnt .= ( get_option( 'main_color' ) !== '#f8f9fa' ) ? '#primary{background-color:' . get_option( 'main_color' ) . ';}' : '';
-	$ekiline_lmnt .= ( get_option( 'menu_color' ) !== '#343a40' ) ? '#primarySiteNavigation.navbar{background-color:' . get_option( 'menu_color' ) . '!important;}' : '';
+	$ekiline_lmnt .= ( get_option( 'main_color' ) && get_option( 'main_color' ) !== '#f8f9fa' ) ? '#primary{background-color:' . get_option( 'main_color' ) . ';}' : '';
+	$ekiline_lmnt .= ( get_option( 'menu_color' ) && get_option( 'menu_color' ) !== '#343a40' ) ? '#primarySiteNavigation.navbar{background-color:' . get_option( 'menu_color' ) . '!important;}' : '';
 
-	if ( get_option( 'fbar_color' ) !== '#6c757d' || get_option( 'fbartxt_color' ) !== '#ffffff' ) {
-		$fb_bg         = ( get_option( 'fbar_color' ) !== '#6c757d' ) ? 'background-color:' . get_option( 'fbar_color' ) . '!important;' : '';
-		$fb_tc         = ( get_option( 'fbartxt_color' ) !== '#ffffff' ) ? 'color:' . get_option( 'fbartxt_color' ) . '!important;' : '';
+	if ( get_option( 'fbar_color' ) && get_option( 'fbar_color' ) !== '#6c757d' || get_option( 'fbartxt_color' ) && get_option( 'fbartxt_color' ) !== '#ffffff' ) {
+		$fb_bg         = ( get_option( 'fbar_color' ) && get_option( 'fbar_color' ) !== '#6c757d' ) ? 'background-color:' . get_option( 'fbar_color' ) . '!important;' : '';
+		$fb_tc         = ( get_option( 'fbartxt_color' ) && get_option( 'fbartxt_color' ) !== '#ffffff' ) ? 'color:' . get_option( 'fbartxt_color' ) . '!important;' : '';
 		$ekiline_lmnt .= '.footer-bar{' . $fb_bg . $fb_tc . '}';
 	}
 
-	$ekiline_lmnt .= ( get_option( 'fbarlks_color' ) !== '#007bff' ) ? '.footer-bar a{color:' . get_option( 'fbarlks_color' ) . '!important}' : '';
+	$ekiline_lmnt .= ( get_option( 'fbarlks_color' ) && get_option( 'fbarlks_color' ) !== '#007bff' ) ? '.footer-bar a{color:' . get_option( 'fbarlks_color' ) . '!important}' : '';
 
-	if ( get_option( 'footer_color' ) !== '#343a40' || get_option( 'ftext_color' ) !== '#ffffff' ) {
-		$foo_bg        = ( get_option( 'footer_color' ) !== '#343a40' ) ? 'background-color:' . get_option( 'footer_color' ) . '!important;' : '';
-		$foo_tc        = ( get_option( 'ftext_color' ) !== '#ffffff' ) ? 'color:' . get_option( 'ftext_color' ) . '!important;' : '';
+	if ( get_option( 'footer_color' ) && get_option( 'footer_color' ) !== '#343a40' || get_option( 'ftext_color' ) && get_option( 'ftext_color' ) !== '#ffffff' ) {
+		$foo_bg        = ( get_option( 'footer_color' ) && get_option( 'footer_color' ) !== '#343a40' ) ? 'background-color:' . get_option( 'footer_color' ) . '!important;' : '';
+		$foo_tc        = ( get_option( 'ftext_color' ) && get_option( 'ftext_color' ) !== '#ffffff' ) ? 'color:' . get_option( 'ftext_color' ) . '!important;' : '';
 		$ekiline_lmnt .= '.site-footer{' . $foo_bg . $foo_tc . '}';
 	}
 
-	$ekiline_lmnt .= ( get_option( 'flinks_color' ) !== '#007bff' ) ? '.site-footer a{color:' . get_option( 'flinks_color' ) . '!important}' : '';
+	$ekiline_lmnt .= ( get_option( 'flinks_color' ) && get_option( 'flinks_color' ) !== '#007bff' ) ? '.site-footer a{color:' . get_option( 'flinks_color' ) . '!important}' : '';
 
 	return $ekiline_lmnt;
 }
